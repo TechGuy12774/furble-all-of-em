f1e3e2e52a15f96e65ad944639d6cace920aea7b
TechGuy12774 <96497031+TechGuy12774@users.noreply.github.com>
2026-05-11 21:45:25 -0500

    Bobby Bungalow
    Bobby Bungalow's Bungalow

--- dlc/index.js
+++ dlc/index.js
@@ -27,5 +27,145 @@
         "buttons": [
             { "title": "Chilli ", "action": [75,0,4,4] }
         ]
+    },
+    {
+        "file": "tr002590.dlc",
+        "title": "DLC tr002590",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr011040.dlc",
+        "title": "DLC tr011040",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr010610.dlc",
+        "title": "DLC tr010610",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr010970.dlc",
+        "title": "DLC tr010970",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr011090.dlc",
+        "title": "DLC tr011090",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr011100.dlc",
+        "title": "DLC tr011100",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr011870.dlc",
+        "title": "DLC tr011870",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu002560.dlc",
+        "title": "DLC tu002560",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu010600.dlc",
+        "title": "DLC tu010600",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu010950.dlc",
+        "title": "DLC tu010950",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu011010.dlc",
+        "title": "DLC tu011010",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu011090.dlc",
+        "title": "DLC tu011090",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu011100.dlc",
+        "title": "DLC tu011100",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu011840.dlc",
+        "title": "DLC tu011840",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr022130.dlc",
+        "title": "DLC tr022130",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tr022140.dlc",
+        "title": "DLC tr022140",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu003150.dlc",
+        "title": "DLC tu003150",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu022130.dlc",
+        "title": "DLC tu022130",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "tu022140.dlc",
+        "title": "DLC tu022140",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "fu001680.dlc",
+        "title": "DLC fu001680",
+        "buttons": [
+            { "title": "Play", "action": [75,0,4,4] }
+        ]
     }
 ]
